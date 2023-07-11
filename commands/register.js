e665c28aee8d73cfdf21f7ae1ed9310aea1e923e
evneis <esn002@lvc.edu>
2023-07-11 18:23:31 -0400

    cleaned up notes

--- commands/register.js
+++ commands/register.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 const axios = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 var Datastore = require('nedb');
-
+var db = new Datastore({filename: `users.db`, autoload: true});
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,5 +17,11 @@ module.exports = {
         //Datastore interactions here
         const username = interaction.options.getString('username');
         const discordUser = interaction.user.id;
+        
+        db.findOne({ discord: `${discordUser}` }, function(err, doc) {
+            if(doc.name != null){
+
+            }
+        });
     },
 };
--- firstDatabaseRun.js
+++ firstDatabaseRun.js
@@ -3,11 +3,29 @@ var Datastore = require('nedb');
 
 var db = new Datastore({filename: `users.db`, autoload: true});
 
-var test2 = {
-    discord: 'axb',
-    username: 'anotha12'
-}
+// var test2 = {
+//     discord: 'axb',
+//     username: 'anotha12'
+// }
+//
+// db.insert(test2, function(err, doc) {
+//     console.log('Inserted', doc.username, 'with ID', doc._id);
+// });
 
-db.insert(test2, function(err, doc) {
-    console.log('Inserted', doc.name, 'with ID', doc._id);
-});
+// db.findOne({ discord: 'abba' }, function(err, doc) {
+//     if(doc.username != null){
+//         console.log('not null!!!');
+//     }else{
+//         console.log('Found user:', doc.username);
+//     }
+// });
+
+
+// db.update({ discord: 'axb' }, { discord: 'axb', username: 'not anotha' }, { upsert: true }, function (err, numReplaced, upsert) {
+//     // numReplaced = 1, upsert = { _id: 'id5', planet: 'Pluton', inhabited: false }
+//     // A new document { _id: 'id5', planet: 'Pluton', inhabited: false } has been added to the collection
+//   });
+
+db.findOne({ discord: 'axb' }, function(err, doc) {
+    console.log(`Found ${doc.username}`)
+})
