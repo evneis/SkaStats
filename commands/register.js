7badc3789b0ddd2bd8a5e873a9a994715d9d1e4e
Evan Neiswender <eneiswender@wyanokegroup.com>
2023-07-12 16:10:10 -0400

    optimizations goin on

--- commands/register.js
+++ commands/register.js
@@ -17,20 +17,29 @@ module.exports = {
         //Datastore interactions here
         const username = interaction.options.getString('username');
         const discordUser = interaction.user.id;
-        var output = 'not replaced';
+        // var output = 'not replaced';
         var flag = false;
-        userdb.db.update({ discord: `${discordUser}` }, { discord: `${discordUser}`, username: `${username}` }, { upsert: true }, function (err, numReplaced, upsert) {
-            if(err){
-                console.log("in error");
-                flag = true;
-                output = "Error ocurred, please try again";
-            }
-        });
-
+        var output = await new Promise((resolve, reject) => {
+            userdb.db.update({ discord: `${discordUser}` }, { discord: `${discordUser}`, username: `${username}` }, { upsert: true }, function (err, numReplaced, upsert) {
+                if(err){
+                    console.log(err);
+                    flag = true;
+                    resolve("Error ocurred, please try again");
+                }else{
+                    console.log(`output recorded? ${output}`);
+                    resolve(`Registered to ${username}`);
+                }
+            });
+        })
+        
+        
+        //TODO: I think we can get rid of all of this and simply reply with output
         if(!flag){
             console.log("no flag");
             output = `Registered to ${username}`;
             console.log(`output recorded? ${output}`);
+            await interaction.reply({content: output, ephemeral: true});
+            return;
         }
 
         // db.findOne({ discord: `${discordUser}` }, function(err, doc) {
