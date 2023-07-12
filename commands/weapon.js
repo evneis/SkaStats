d62f1b02442b314dc6d21b1ac304fd930935daaf
Evan Neiswender <eneiswender@wyanokegroup.com>
2023-07-12 15:42:19 -0400

    weapon works but is lowkey super sluggish now...

---
+++ commands/weapon.js
@@ -0,0 +1,78 @@
+// const globalMethods = require("../globalMethods")
+var userdb = require("../db");
+require('dotenv').config();
+const fs = require('fs').promises;
+const axios = require('axios');
+const { SlashCommandBuilder, EmbedBuilder, Client, GatewayIntentBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('weapon')
+        .setDescription('Returns statistics for a provided map')
+        .addStringOption(option =>
+            option.setName('weapon')
+                .setDescription('The weapon you would like to see stats for')
+                .setRequired(true)),
+    async execute(interaction) {
+        const weap = interaction.options.getString('weapon');
+        var embedded;
+        var notEmbed;
+        var flag = false;
+        var username = await new Promise((resolve, reject) => {
+            userdb.db.findOne({ discord: `${interaction.user.id}` }, function (err, doc) {
+                if (doc) {
+                    username = doc.username;
+                    console.log(doc.username);
+                    resolve(doc.username);
+                } else {
+                    console.log("no doc");
+                }
+            });
+
+        });
+
+        let resp = await axios({
+            url: `https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${username}/segments/weapon`,
+            headers: { "TRN-Api-Key": process.env.TRN_API_KEY, },
+            method: 'get',
+        }).then(response => {
+            console.log(response.data);
+            var respList = response.data.data;
+            for (var i = 0; i < respList.length; i++) {
+                if (weap.toLowerCase() === 'list') {
+                    flag = true;
+                    var allWeaps = `Available Weapons:
+                    `;
+                    for (var j = 0; j < respList.length; j++) {
+                        allWeaps = allWeaps.concat(`${respList[j].metadata.name}
+                        `);
+                    }
+                    notEmbed = allWeaps;
+                    break;
+                }
+
+                var obj = respList[i];
+                const objName = obj.metadata.name;
+                if (obj.metadata.name.toLowerCase() === weap.toLowerCase()) {
+                    embedded = new EmbedBuilder()
+                        .setTitle(`${obj.metadata.name} stats for ${interaction.user.username}`)
+                        .addFields({ name: `Kills`, value: `${obj.stats.kills.displayValue}` },
+                            { name: `Shots Fired`, value: `${obj.stats.shotsFired.displayValue}` },
+                            { name: `Accuracy`, value: `${obj.stats.shotsAccuracy.displayValue}` })
+                        .setImage(`${obj.metadata.imageUrl}`);
+                    console.log(`${obj.metadata.name}`);
+                    break;
+                }
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+
+        if (!embedded) embedded = `CUCK!`;
+
+        if (flag)
+            await interaction.reply(notEmbed);
+        else
+            await interaction.reply({ embeds: [embedded] });
+    },
+};
