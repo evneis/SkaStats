f1764308b4832279d43cd2ce3a7440a3d6f655a9
Evan Neiswender <eneiswender@wyanokegroup.com>
2023-07-12 11:12:33 -0400

    register works? im skeptical

--- commands/register.js
+++ commands/register.js
@@ -17,18 +17,21 @@ module.exports = {
         //Datastore interactions here
         const username = interaction.options.getString('username');
         const discordUser = interaction.user.id;
-        var output;
-        
+        var output = 'not replaced';
+        var flag = false;
         db.update({ discord: `${discordUser}` }, { discord: `${discordUser}`, username: `${username}` }, { upsert: true }, function (err, numReplaced, upsert) {
             if(err){
                 console.log("in error");
+                flag = true;
                 output = "Error ocurred, please try again";
             }
-            else{
-                
-                output = `Successfully Registered ${interaction.user.username} to ${username}`; 
-            }
         });
+
+        if(!flag){
+            console.log("no flag");
+            output = `Registered to ${username}`;
+            console.log(`output recorded? ${output}`);
+        }
 
         // db.findOne({ discord: `${discordUser}` }, function(err, doc) {
         //     if(doc.name != null){
@@ -36,6 +39,6 @@ module.exports = {
         //     }
         // });
         if(!output){output = 'CUCKO!';}
-        await interaction.reply(output);
+        await interaction.reply({ content: output, ephemeral: true });
     },
 };
