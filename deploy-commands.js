0c76a0bfcc56c7fc93076cee26280fd6d9df0711
Evan Neiswender <eneiswender@wyanokegroup.com>
2023-07-12 17:38:59 -0400

    notes for next adventure

--- deploy-commands.js
+++ deploy-commands.js
@@ -32,6 +32,7 @@ const rest = new REST().setToken(process.env.DISCORD_BOT_ID);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
+			// TODO: make this global if possible
 			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
